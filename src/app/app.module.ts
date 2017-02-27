7062533a1a0e7beb2e15fc9b6af2d75aeb46580c
FallDownTheSystem <jere.kaplas@gmail.com>
2017-02-27 14:17:36 +0200

    Adds basic routing.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
+import 'hammerjs';
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { MaterialModule } from '@angular/material';
 
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 
-import 'hammerjs';
+import { AppComponent } from './app.component';
 import { RegistrationComponent } from './registration/registration.component';
 
 @NgModule({
@@ -18,7 +20,8 @@ import { RegistrationComponent } from './registration/registration.component';
     BrowserModule,
     FormsModule,
     HttpModule,
-    MaterialModule
+    MaterialModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
