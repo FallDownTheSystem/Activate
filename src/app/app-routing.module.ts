e32f7b28c22ea06e74008bb7c5fc83ac10c37e31
FallDownTheSystem <jere.kaplas@gmail.com>
2017-03-08 14:12:24 +0200

    Edited html and css for access page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AccessFormsComponent } from './access-forms/access-forms.component';
+import { ActivityCardComponent } from './activity-card/activity-card.component';
 
 const routes: Routes = [
-  // { path: '', redirectTo: '/', pathMatch: 'full' },
-  { path: 'access',  component: AccessFormsComponent }
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'access',  component: AccessFormsComponent },
+  { path: 'home',  component: ActivityCardComponent }
 ];
 @NgModule({
   imports: [ RouterModule.forRoot(routes) ],
