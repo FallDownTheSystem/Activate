23f311de7fb7dc472453480eca90f7731b17c50a
FallDownTheSystem <jere.kaplas@gmail.com>
2017-03-13 18:38:40 +0200

    Fixes animations from breaking change in Angular 4.0.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { MaterialModule } from '@angular/material';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
@@ -60,6 +60,7 @@ export const firebaseConfig = {
 		ReactiveFormsModule,
 		HttpModule,
 		MaterialModule,
+		BrowserAnimationsModule,
 		AppRoutingModule,
 		StoreModule.provideStore(reducer),
 		EffectsModule.run(CategoryEffects),
