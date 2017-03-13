e9eeb41e50c35b04517a38f1a6a0be033548a9f2
Kukkaherra <kukkaherra@gmail.com>
2017-03-14 00:19:52 +0200

    Fixed listing to use new location of activity class

--- src/app/activity-card/activity-card.component.ts
+++ src/app/activity-card/activity-card.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs/Observable';
-import { Activity } from '../services/activity.service';
+import { Activity } from '../model/activity';
 import { AppStore } from '../store/app-store';
 
 @Component({
