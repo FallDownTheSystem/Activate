e4931b7e19a2003f803195b5edbba67136a697ff
Kukkaherra <kukkaherra@gmail.com>
2017-03-15 15:08:43 +0200

    Main page animations

--- src/app/components/activity-card/activity-card.component.ts
+++ src/app/components/activity-card/activity-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, trigger, state, style, transition, animate, keyframes } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs/Observable';
 import { Activity } from '../../model/activity';
@@ -8,6 +8,64 @@ import { AppStore } from '../../store/app-store';
 	selector: 'act-activity-card',
 	templateUrl: './activity-card.component.html',
 	styleUrls: ['./activity-card.component.scss'],
+	animations: [
+		trigger('entry', [
+			transition('void => *', [
+				animate(100, keyframes([
+					style({opacity: 0, transform: 'translateX(-100%)', offset: 0}),
+					style({opacity: 1, transform: 'translateX(0)', offset: 1}),
+				]))
+			])
+		]),
+		trigger('select', [
+			transition('void => *', [
+				animate(150, keyframes([
+					style({transform: 'translateX(-70%) scale(0.3)', offset: 0}),
+					style({transform: 'translateX(0) scale(1)', offset: 1}),
+				]))
+			]),
+			transition('* => void', [
+				animate(150, keyframes([
+					style({opacity: 1, transform: 'translateX(0) scale(1)', offset: 0}),
+					style({opacity: 0, transform: 'translateX(-70%) scale(0.3)', offset: 0.8}),
+					style({opacity: 0, transform: 'translateX(-70%) scale(0)', offset: 1}),
+				]))
+			]),
+			transition('* => *', [
+				animate(300, keyframes([
+					style({opacity: 1, transform: 'scale(1)', offset: 0}),
+					style({opacity: 0, transform: 'scale(0.3)', offset: 0.3}),
+					style({opacity: 1, transform: 'scale(1)', offset: 1}),
+				]))
+			])
+		]),
+		trigger('select-mobile', [
+			transition('void => *', [
+				animate(150, keyframes([
+					style({opacity: 0, transform: 'translateY(-20%)', marginBottom: '-20%', offset: 0}),
+					style({opacity: 1, transform: 'translateY(0)', marginBottom: '0', offset: 1}),
+				]))
+			]),
+			transition('* => void', [
+				animate(150, keyframes([
+					style({opacity: 1, transform: 'translateY(0)', marginBottom: '0', offset: 0}),
+					style({opacity: 0, transform: 'translateY(-20%)', marginBottom: '-20%', offset: 1}),
+				]))
+			])
+		]),
+		trigger('mobile', [
+			state("mobile", style({
+				width: '100%',
+				maxWidth: '100%'
+			})),
+			state('desktop', style({
+				width: '45%'
+			})),
+			transition('desktop <=> mobile', [
+				animate(300)
+			])
+		])
+	],
 	host: {
 		'(window:resize)': 'onResize()'
 	}
@@ -18,6 +76,7 @@ export class ActivityCardComponent implements OnInit, OnDestroy {
 	subcription: any;
 	selectedActivity: Activity;
 	mobileView: boolean;
+	view: string;
 
 	constructor(private store: Store<AppStore>) {
 		this.activitiesObs = store.select(s => s.activities);
@@ -43,8 +102,9 @@ export class ActivityCardComponent implements OnInit, OnDestroy {
 	}
 
 	onResize() {
-		this.mobileView = false;
-		if (window.innerWidth < 850) this.mobileView = true;
+		if (window.innerWidth <= 850) this.mobileView = true;
+		else if (window.innerWidth > 850) this.mobileView = false;
+		this.view = (this.mobileView === false ? "desktop" : "mobile");
 	}
 
 }
--- src/app/rxjs-extensions.ts
+++ src/app/rxjs-extensions.ts
@@ -12,3 +12,4 @@ import 'rxjs/add/operator/filter';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/switchMap';
 import 'rxjs/add/operator/take';
+import 'rxjs/add/operator/startWith';
