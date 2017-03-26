a7178467e5084afc66bff308f85c6209d84dc9bd
Kukkaherra <kukkaherra@gmail.com>
2017-03-26 17:00:33 +0300

    Message component

--- src/app/components/activity-card/message/message.component.ts
+++ src/app/components/activity-card/message/message.component.ts
@@ -7,6 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MessageComponent implements OnInit {
 
+
+	messages = [
+		{
+			user: "Owner",
+			owner: true,
+			timestamp: "2017-02-02 - 12:00",
+			message: "asd asd asdasd asdasdasd asd asd asdasd asdasdasd asd asd asdasd asdasdasd asd asd asdasd asdasdasd \
+			 asd asd asdasd asdasdasd asd asd asdasd asdasdasd asd asd asdasd"
+		},
+		{
+			user: "Someone else else",
+			owner: false,
+			timestamp: "2017-02-02 - 13:00",
+			message: "asd asd asdasd asdasdasd asd asd asdasd asdasdasd asd asd asdasd asdasdasd asd asd asdasd asdasdasd "
+		},
+		{
+			user: "Someone",
+			owner: false,
+			timestamp: "2017-02-02 - 13:13",
+			message: "asd asd asdasd asdasdasd asd asd asdasd asdasdasd asd asd asdasd asdasdasd asd asd asdasd asdasdasd "
+		},
+	]
+
   constructor() { }
 
   ngOnInit() {
