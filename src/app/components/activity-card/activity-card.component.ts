963173ae9acc98e6e4f353ec8f9354b042672780
Kukkaherra <kukkaherra@gmail.com>
2017-03-27 22:37:53 +0300

    Animations and app background

--- src/app/components/activity-card/activity-card.component.ts
+++ src/app/components/activity-card/activity-card.component.ts
@@ -1,77 +1,19 @@
 import { Filter } from '../../model/filter';
 import { ActivityActions } from '../../store/actions/activity.actions';
 import { User } from '../../model/user';
-import { Component, OnInit, OnDestroy, HostListener, trigger, state, style, transition, animate, keyframes } from '@angular/core';
+import { Component, OnInit, OnDestroy, HostListener } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs/Observable';
 import { Activity } from '../../model/activity';
 import { AppStore } from '../../store/app-store';
 import '../../rxjs-extensions';
+import { animations } from './animations';
 
 @Component({
 	selector: 'act-activity-card',
 	templateUrl: './activity-card.component.html',
 	styleUrls: ['./activity-card.component.scss'],
-	animations: [
-		trigger('entry', [
-			transition('void => *', [
-				animate(100, keyframes([
-					style({opacity: 0, transform: 'translateX(-100%)', offset: 0}),
-					style({opacity: 1, transform: 'translateX(0)', offset: 1}),
-				]))
-			])
-		]),
-		trigger('select', [
-			transition('void => *', [
-				animate(150, keyframes([
-					style({transform: 'translateX(-70%) scale(0.3)', offset: 0}),
-					style({transform: 'translateX(0) scale(1)', offset: 1}),
-				]))
-			]),
-			transition('* => void', [
-				animate(150, keyframes([
-					style({opacity: 1, transform: 'translateX(0) scale(1)', offset: 0}),
-					style({opacity: 0, transform: 'translateX(-70%) scale(0.3)', offset: 0.8}),
-					style({opacity: 0, transform: 'translateX(-70%) scale(0)', offset: 1}),
-				]))
-			]),
-			transition('* => *', [
-				animate(300, keyframes([
-					style({opacity: 1, transform: 'scale(1)', offset: 0}),
-					style({opacity: 0, transform: 'scale(0.3)', offset: 0.3}),
-					style({opacity: 1, transform: 'scale(1)', offset: 1}),
-				]))
-			])
-		]),
-		trigger('select-mobile', [
-			transition('* => *', [
-				animate(100, keyframes([
-					style({height: '*', offset: 0}),
-					style({height: '*', offset: 1}),
-				]))
-			])
-		]),
-		trigger('select-mobile-out', [/*
-			transition('void => *', [
-				animate(50, keyframes([
-					style({opacity: 0.2, offset: 0}),
-					style({opacity: 1, offset: 1}),
-				]))
-			])
-		*/]),
-		trigger('mobile', [
-			state('mobile', style({
-				width: '100%',
-				maxWidth: '100%'
-			})),
-			state('desktop', style({
-				width: '45%'
-			})),
-			transition('desktop <=> mobile', [
-				animate(300)
-			])
-		])
-	]
+	animations: [animations]
 })
 
 
@@ -115,7 +57,12 @@ export class ActivityCardComponent implements OnDestroy {
 		}
 	}
 
-	select(i) {
+	select(i, listItem, page) { 
+		/*
+			These variables may be used in the future to set mobile screen position
+			when expanding / collapsing elements would cause clicked item to go out of screen
+		*/
+		
 		// We can either remember that comments should be shown, or hide comments when selecting a new activity
 		this.showComments = false;
 		if (this.selectedActivity === this.activities[i]) {
@@ -124,6 +71,14 @@ export class ActivityCardComponent implements OnDestroy {
 			this.selectedActivity = null; // set to null before selecting anyways to clear comments and shiz
 			this.selectedActivity = this.activities[i];
 		}
+		/*
+		if (this.mobileView) {
+			setTimeout(() => {
+				listItem.scrollIntoView();
+				page.scrollTop -= 20;
+			}, 100)
+		}
+		*/
 	}
 
 	stopProp(event) {
---
+++ src/app/components/activity-card/animations.ts
@@ -0,0 +1,46 @@
+import { trigger, state, style, transition, animate, keyframes } from '@angular/core';
+
+export const animations = [
+	trigger('entry', [
+		transition('void => *', [
+			animate(100, keyframes([
+				style({opacity: 0, transform: 'translateX(-100%)', offset: 0}),
+				style({opacity: 1, transform: 'translateX(0)', offset: 1}),
+			]))
+		])
+	]),
+	trigger('select', [
+		transition('void => *', [
+			animate(150, keyframes([
+				style({transform: 'translateX(-70%) scale(0.3)', offset: 0}),
+				style({transform: 'translateX(0) scale(1)', offset: 1}),
+			]))
+		]),
+		transition('* => void', [
+			animate(150, keyframes([
+				style({opacity: 1, transform: 'translateX(0) scale(1)', offset: 0}),
+				style({opacity: 0, transform: 'translateX(-70%) scale(0.3)', offset: 0.8}),
+				style({opacity: 0, transform: 'translateX(-70%) scale(0)', offset: 1}),
+			]))
+		]),
+		transition('* => *', [
+			animate(300, keyframes([
+				style({opacity: 1, transform: 'scale(1)', offset: 0}),
+				style({opacity: 0, transform: 'scale(0.3)', offset: 0.3}),
+				style({opacity: 1, transform: 'scale(1)', offset: 1}),
+			]))
+		])
+	]),
+	trigger('mobile', [
+		state('mobile', style({
+			width: '100%',
+			maxWidth: '100%'
+		})),
+		state('desktop', style({
+			width: '45%'
+		})),
+		transition('desktop <=> mobile', [
+			animate(300)
+		])
+	]),
+]
